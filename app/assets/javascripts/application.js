9cd2954d6bb43010124e9e5da0a25e1a69994a79
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-02-06 11:00:30 -0600

    Changed libraries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require libs/jquery
+//= require libs/rails
+//= require libs/underscore
+//= require libs/backbone
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    @articles = Article.limit(3)
   end
 end
