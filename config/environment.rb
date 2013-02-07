89c36b2ec9a558cb8b9e0f6768033f975919a325
Juan Carlos <juancarlos@tangosource.com>
2013-02-07 14:06:48 -0600

    modify the file environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 CRUDExample::Application.initialize!
 
+config.gem "authlogic"
 config.gem "declarative_authorization", :source => "http://gemcutter.org"
